da1e9eff0a527b19109afe8048627ec5adcd543b
nisaakamila <nisaa.kamila@bening-semesta.com>
2024-05-30 12:54:51 +0700

    Waktu Makan add name

--- src/main/java/com/example/repository/WaktuMakanRepository.java
+++ src/main/java/com/example/repository/WaktuMakanRepository.java
@@ -1,5 +1,7 @@
 package com.example.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import com.example.model.WaktuMakan;
@@ -9,9 +11,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WaktuMakanRepository extends JpaRepository<WaktuMakan, Long>{
+    Optional<WaktuMakan> findByNama(String nama);
     // Paging and sorting
     Page<WaktuMakan> findAll(Pageable pageable);
 
     // Searching
     Page<WaktuMakan> findByNamaContaining(String nama, Pageable pageable);
+
+    
 }
